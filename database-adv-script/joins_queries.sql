f3300c862dfb1eb3f0e400fbeb0ee704f164f94f
Engineer Tony <wangolotony4@gmail.com>
2025-09-06 21:35:50 +0300

    Add ORDER BY to LEFT JOIN query (properties then reviews)

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -5,10 +5,15 @@ FROM bookings b
 INNER JOIN users u ON b.user_id = u.id;
 
 -- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
-SELECT p.id AS property_id, p.name AS property_name,
-       r.id AS review_id, r.rating, r.comment
+SELECT 
+  p.id AS property_id,
+  p.name AS property_name,
+  r.id AS review_id,
+  r.rating,
+  r.comment
 FROM properties p
-LEFT JOIN reviews r ON p.id = r.property_id;
+LEFT JOIN reviews r ON p.id = r.property_id
+ORDER BY p.id ASC, r.id ASC;
 
 -- 3. FULL OUTER JOIN: Retrieve all users and all bookings, even if no match
 
